3fc2d3fdd7e53cdd9cf6dd0b34aecfca049d21c3
Celestin <tiegocelex@gmail.com>
2025-04-12 18:45:48 +0200

    Add files via upload

---
+++ site.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p> Celestin testing now  Learning php </p>";
+        ?>
+    </body>
+</html>
